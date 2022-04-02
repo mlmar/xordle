cfaed76e20f3dbb143a00552fafd3aa66c92868d
mlmar <marcloussm@gmail.com>
2022-04-01 19:14:31 -0700

    changed deployment url

--- xordle-vite/src/util/SystemUtil.js
+++ xordle-vite/src/util/SystemUtil.js
@@ -1,4 +1,4 @@
 const DEV = false;
 
-export const SERVER_URL = DEV ? "http://localhost:3300" : "https://xordle.herokuapp.com";
-export const SOCKET_URL = DEV ? "ws://localhost:3300" : "wss://xordle.herokuapp.com";
+export const SERVER_URL = DEV ? "http://localhost:3300" : "https://xordle.onrender.com";
+export const SOCKET_URL = DEV ? "ws://localhost:3300" : "wss://xordle.onrender.com";
