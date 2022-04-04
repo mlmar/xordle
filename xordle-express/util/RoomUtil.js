475ccfc9a13faf0772d1c44448f8479bbf94735d
marcus <marcloussm@gmail.com>
2022-04-04 03:10:46 -0700

    fixed set deletion

--- xordle-express/util/RoomUtil.js
+++ xordle-express/util/RoomUtil.js
@@ -179,7 +179,7 @@ class Room {
 
   removeOldest() {
     const word = this.history.shift();
-    this.historySet.delete(word);
+    this.historySet.delete(word.map(({ letter }) => letter).join(''));
     this.keys = {};
     this.history.forEach((word) => {
       word.forEach(({ letter, status })  => {
