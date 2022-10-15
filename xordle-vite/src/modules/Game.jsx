11c9e076df43f226cdd220576009225744b66498
marcus <marcloussm@gmail.com>
2022-10-15 05:38:47 -0700

    Fixing blinking

--- xordle-vite/src/modules/Game.jsx
+++ xordle-vite/src/modules/Game.jsx
@@ -18,7 +18,7 @@ const Game = (props) => {
     socketUtil.listen('JOIN', setGameData);
     socketUtil.listen('UPDATE', setGameData);
     socketUtil.listen('PLAYER_UPDATE', (playerData) => {
-      if(playerData?.history[playerData?.history?.length - 1] === this.current.join('')) {
+      if(playerData?.history[playerData?.history?.length - 1] === current.join('')) {
         setCurrent([])
       }
       setPlayerData(playerData);
