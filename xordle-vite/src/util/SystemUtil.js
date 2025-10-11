a820b044432ef1721fafff50c7070a067db0649f
mlmar <marcloussm@gmail.com>
2025-10-11 16:48:53 -0700

    Fixing server url to point to new render url

--- xordle-vite/src/util/SystemUtil.js
+++ xordle-vite/src/util/SystemUtil.js
@@ -2,7 +2,7 @@ export const DEV = false;
 
 const LOCAL = 'localhost:3300';
 // const LOCAL = '192.168.0.87:3300';
-const DEPLOYED = 'xordle.app';
+const DEPLOYED = 'xordle-wwnx.onrender.com';
 
 export const SERVER_URL = DEV ? 'http://' + LOCAL : 'https://' + DEPLOYED;
 export const SOCKET_URL = DEV ? 'ws://' + LOCAL : 'wss://' + DEPLOYED;
